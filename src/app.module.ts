1d408e9ce2cd9cec1139f6f1d9fef5c8210be32a
ilya <kotena1980@bk.ru>
2025-08-24 16:03:59 +0500

    create numbers essence

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { NumbersModule } from './numbers/numbers.module';
 
 @Module({
-  imports: [],
+  imports: [NumbersModule],
 })
 export class AppModule {}
