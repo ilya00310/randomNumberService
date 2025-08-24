7c343fec35249e5e27fd921a4734cd5fb0d0c68d
ilya <kotena1980@bk.ru>
2025-08-24 18:28:52 +0500

    add endpoint create random number

--- src/numbers/numbers.controller.ts
+++ src/numbers/numbers.controller.ts
@@ -1,7 +1,12 @@
-import { Controller } from "@nestjs/common";
+import { Controller, Post } from "@nestjs/common";
 import { NumbersService } from "./numbers.service";
 
 @Controller("numbers")
 export class NumbersController {
   constructor(private readonly numbersService: NumbersService) {}
+
+  @Post("random")
+  createRandom() {
+    return this.numbersService.createRandom();
+  }
 }
--- src/numbers/numbers.service.ts
+++ src/numbers/numbers.service.ts
@@ -4,4 +4,13 @@ import { PrismaService } from "../database/prisma.service";
 @Injectable()
 export class NumbersService {
   constructor(private prismaService: PrismaService) {}
+
+  async createRandom() {
+    const randomNumber = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+    return await this.prismaService.numbers.create({
+      data: {
+        number: randomNumber,
+      },
+    });
+  }
 }
